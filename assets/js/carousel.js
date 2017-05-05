0269a63c803fa6a878bf0ba1b8ef4aa0aa6050d0
CalebCox <calebmk8190@gmail.com>
2017-05-05 13:02:21 -0400

    v0.3.1 - first push to remote repo.

---
+++ assets/js/carousel.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.partner-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        variableWidth: false,
+        infinite: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
